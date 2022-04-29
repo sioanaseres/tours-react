2e6a9a5a3b4d0815f1f9867b66c2acf584d6b309
Ioana Cosoreanu Seres <79806330+sioanaseres@users.noreply.github.com>
2022-04-29 22:37:14 +0300

    Add files via upload

---
+++ build/precache-manifest.a7b9a9aa82275ac274b94ca36b1a5a1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9b7c4446109a870737c4922341f083d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a579ca861db087fd744",
+    "url": "/static/css/main.a173c0c0.chunk.css"
+  },
+  {
+    "revision": "303a7e45ea08ea333814",
+    "url": "/static/js/2.b79bb511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b79bb511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a579ca861db087fd744",
+    "url": "/static/js/main.b4cb8a04.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
